22b20788bcb90db2e93c8301036a1aa1d947208d
James C. Davis <jdavis@lighthouse.tamucc.edu>
2011-10-07 16:49:58 -0500

    Added ability to add a message

--- lib/IRC/Bot/ESP.pm
+++ lib/IRC/Bot/ESP.pm
@@ -15,7 +15,7 @@ sub said {
 
     return if ($self->ignore_nick($args->{who}));
 
-    if (my ($nick) = $args->{body} =~ /^([\w\-]+):\s*esping/) {
+    if (my ($nick,$msg) = $args->{body} =~ /^([\w\-]+):\s*esping(.*)$/) {
         return "$args->{who}: sorry, I don't know how to electro-shock ping $nick" unless exists $self->{nickmap}->{$nick};
         return "$args->{who}: sorry, I don't know $nick\'s number" unless exists $self->{nickmap}->{$nick}->{number};
         return "$args->{who}: sorry, I don't know $nick\'s carrier" unless exists $self->{nickmap}->{$nick}->{carrier};
@@ -24,6 +24,8 @@ sub said {
         print SENDMAIL "To: $self->{nickmap}->{$nick}->{number}\@$self->{carriermap}->{$self->{nickmap}->{$nick}->{carrier}}\n";
         print SENDMAIL "Content-type: text/plain\n\n";
         print SENDMAIL "$args->{who} is electro-shock pinging you on $args->{channel}";
+        $msg =~ s/^\s+//;
+        print SENDMAIL ": $msg" if $msg;
         close(SENDMAIL);
         return "$args->{who}: electro-shock pinging $nick";
     }
